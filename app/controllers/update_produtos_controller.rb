1607f7dab8864cfa59958b925ec174fb43c2a5a6
Jairo Gimenes <jagimenes@gmail.com>
2017-05-23 22:28:14 -0300

    Alterações

--- app/controllers/update_produtos_controller.rb
+++ app/controllers/update_produtos_controller.rb
@@ -2,7 +2,7 @@ class UpdateProdutosController < ApplicationController
   respond_to :json
 
   def index
-    @produtos = Produto.joins(:grupos).where('grupos.especialidade_id = ?', params[:especialidade_id])
+    @produtos = Produto.joins(:grupos).where('grupos.especialidade_id = ?', params[:especialidade_id]).uniq
     respond_with(@produtos.sort_by{|e| e[:descricao]})
   end
 
