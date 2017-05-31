29eb36158e8594e6844c8db8842eefd7c2f6363e
Jairo Gimenes <jagimenes@gmail.com>
2017-05-30 21:58:33 -0300

    Mudanças no sistema

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,21 +27,21 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
         if @user.update_attributes(secure_params)
-        usuario = params[:user].to_a
-        if usuario[0][1].to_s == "true"
-           @permissoes = @user.permissaos
-           @permissoes.destroy_all
-           @grupos = Especialidade.all
-           for grupo in @grupos
-              @permissao = Permissao.new
-              @permissao.user_id = params[:id]
-              @permissao.especialidade_id = grupo.id
-              puts @permissao.to_s
-              @permissao.save
-           end
-           @user.permissaos = @permissoes
-           @user.save
-        end
+        #usuario = params[:user].to_a
+        #if usuario[0][1].to_s == "true"
+        #   @permissoes = @user.permissaos
+        #   @permissoes.destroy_all
+        #   @grupos = Especialidade.all
+        #   for grupo in @grupos
+        #      @permissao = Permissao.new
+        #      @permissao.user_id = params[:id]
+        #      @permissao.especialidade_id = grupo.id
+        #      puts @permissao.to_s
+        #      @permissao.save
+        #   end
+        #   @user.permissaos = @permissoes
+        #   @user.save
+        #end
         redirect_to users_path, :notice => "Usuário Atualizado."
       else
         redirect_to users_path, :alert => "Não foi possível atualizar o usuário."
