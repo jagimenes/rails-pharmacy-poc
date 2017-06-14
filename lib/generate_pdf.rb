954c7157beefe393413b7d1d3d9b88b155adbf74
Jairo Gimenes <jagimenes@gmail.com>
2017-06-13 23:41:20 -0300

    Alterações flaviano

--- lib/generate_pdf.rb
+++ lib/generate_pdf.rb
@@ -17,8 +17,11 @@ module GeneratePdf
     contador = 1
     imprime_contador = true
     #contador_ingredientes = 1
-    
-    PDF_OPTIONS[:margin] = [usuario.superior, usuario.lateral]
+    if usuario.superior.empty or usuario.lateral.empty
+      PDF_OPTIONS[:margin] = [40, 75]
+    else
+      PDF_OPTIONS[:margin] = [usuario.superior, usuario.lateral]
+    end
     Prawn::Document.new(PDF_OPTIONS) do |pdf|
       # Define a cor do traçado
       pdf.fill_color "666666"
