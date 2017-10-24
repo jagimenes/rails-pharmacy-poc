dcc98121cba1e6f5073e13e5f0a6d6109992ce65
Jairo GImenes <jagimenes@gmail.com>
2017-10-24 09:24:19 -0200

    Windows test

--- app/controllers/update_veiculo_formula_controller.rb
+++ app/controllers/update_veiculo_formula_controller.rb
@@ -4,7 +4,7 @@ class UpdateVeiculoFormulaController < ApplicationController
   def index
     @veiculos = Formula.select('veiculos.id, veiculos.nome, formulas.quantidade, (select unidades.id from unidades where unidade_id = formulas.unidade_id) as veiculo_id, (select unidades.unidade from unidades where unidade_id = formulas.unidade_id) as veiculo_unidade ').joins(:veiculo).where(' formulas.id = ?  ', params[:formula_id] ).distinct
     if @veiculos.length == 0
-    	@veiculos = Veiculo.select('id, nome, 0 as quantidade, 0 as veiculo_unidade, 0 as veiculo_id').all.distinct
+    	@veiculos = Veiculo.select('id, nome, 0 as quantidade, 0 as veiculo_unidade, 0 as veiculo_id').distinct.all
     end
     respond_with(@veiculos.sort_by{|e| e[:nome]})
   end
