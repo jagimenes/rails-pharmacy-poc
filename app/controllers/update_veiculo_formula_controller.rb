9028cf93179b8ba6769f3254bbcea427f880df2a
Jairo GImenes <jagimenes@gmail.com>
2017-10-24 09:42:52 -0200

    teste 3

--- app/controllers/update_veiculo_formula_controller.rb
+++ app/controllers/update_veiculo_formula_controller.rb
@@ -2,7 +2,7 @@ class UpdateVeiculoFormulaController < ApplicationController
   respond_to :json
 
   def index
-	  if !params[:formula_id].empty
+	  if params[:formula_id].size > 0
 		@veiculos = Formula.select('veiculos.id, veiculos.nome, formulas.quantidade, unidades.id as veiculo_id, unidades.unidade as veiculo_unidade ').joins(:veiculo).joins(:unidade).where(' formulas.id = ?  ', params[:formula_id] ).distinct
 		if @veiculos.length == 0
 			@veiculos = Veiculo.select('id, nome, 0 as quantidade, 0 as veiculo_unidade, 0 as veiculo_id').distinct.all
