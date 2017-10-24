7a6fb499aff2547db08bae7c48b3c6b7349cd86d
Jairo GImenes <jagimenes@gmail.com>
2017-10-24 09:45:10 -0200

    teste 4

--- app/controllers/update_ingredientes_controller.rb
+++ app/controllers/update_ingredientes_controller.rb
@@ -2,12 +2,14 @@ class UpdateIngredientesController < ApplicationController
   respond_to :json
 
   def index
-    @ingredientes = Ingrediente.select('ingredientes.produto_id AS produto_id, 
-    								    produtos.descricao as produto_descricao,
-    								    ingredientes.unidade_id AS unidade_id,
-    								    unidades.unidade as unidade,
-    								    formulas.editar as editar,
-    								    ingredientes.quantidade').joins(:unidade).joins(:formula).joins(:produto).where('ingredientes.formula_id = ?', params[:formula_id])
-    respond_with(@ingredientes)
+	if params[:formula_id].size > 0
+		@ingredientes = Ingrediente.select('ingredientes.produto_id AS produto_id, 
+											produtos.descricao as produto_descricao,
+											ingredientes.unidade_id AS unidade_id,
+											unidades.unidade as unidade,
+											formulas.editar as editar,
+											ingredientes.quantidade').joins(:unidade).joins(:formula).joins(:produto).where('ingredientes.formula_id = ?', params[:formula_id])
+		respond_with(@ingredientes)
+	end
   end
 end
